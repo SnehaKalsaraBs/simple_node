458fa3c157beced1da0aa13d5e8a2ddc7e6ae734
Sneha Kalsara <sneha@bigscal.com>
2022-01-11 11:00:15 +0530

    new project

--- app.js
+++ app.js
@@ -7,63 +7,8 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get('/', async (req, res) => {
 
-  console.log('book management github..');
+  res.send('book management github..');
 })
-
-
-// app.get('/show/:id', async (req, res) => {
-
-//   try {
-//     const id = req.params.id
-//     const data = await Books.findById(id);
-//     res.send(data);
-//   }catch(error) {
-//     res.status(400).send(error);
-//     console.log(error);
-//   }
-// })
-
-// app.post('/add', async (req, res) => {
-
-//   try {
-
-//     const payload = req.body;
-//     await Books.create(payload);
-//     res.send("Data added successfully");
-
-//   } catch (error) {
-//     res.status(400).send(error);
-//     console.log(error);
-//   }
-// })
-
-// app.put('/update/:id', async (req, res) => {
-
-//   try {
-//     const id = req.params.id;
-//     const payload = req.body;
-//     await Books.findByIdAndUpdate(id, payload);
-//     res.send("Data updated successfully");
-
-//   } catch (error) {
-//     res.status(400).send(error);
-//     console.log(error);
-//   }
-// })
-
-// app.delete('/delete/:id', async (req, res) => {
-
-//   try {
-//     const id = req.params.id;
-//     await Books.findOneAndDelete(id);
-//     res.send("Data deleted successfully");
-
-//   } catch (error) {
-//     res.status(400).send(error);
-//     console.log(error);
-//   }
-// })
-
 
 const server = app.listen(5000,()=>{console.log("server listening..");})
 
--- test/book.spec.js
+++ test/book.spec.js
@@ -9,90 +9,15 @@ describe('Books api', () => {
 
   describe('GET /', function() {
 
-    it('It should get all the books', (done) => {
+    it('First Page', (done) => {
       chai.request(server)
           .get('/')
           .end((err, response) => {
             response.should.have.status(200);
-            response.body.should.be.a('object');
+            response.text.should.be.eq('book management github..');
             done();
           })
     });
 
   });
-
-  describe('GET /show/:id', function() {
-
-    it('It should get books by id', (done) => {
-      let bookId = '61dbd00391ce0f7704d32ece'
-      chai.request(server)
-          .get('/show/'+ bookId)
-          .end((err, response) => {
-            response.should.have.status(200);
-            response.body.should.be.a('object');
-            response.body.should.have.property('_id');
-            response.body.should.have.property('title');
-            response.body.should.have.property('author');
-            response.body.should.have.property('__v');
-            done();
-          })
-    });
-
-  });
-
-  describe('POST /show/:id', function() {
-
-    it('It should add new book', (done) => {
-      const book = {
-        title: "abc",
-        author: "pqr"
-      }
-      chai.request(server)
-          .post('/add')
-          .send(book)
-          .end((err, response) => {
-            response.should.have.status(200);
-            response.text.should.be.eq('Data added successfully');
-            done();
-          })
-    });
-  });
-
-
-  describe('PUT /update/:id', function() {
-
-    it('It should update existing book', (done) => {
-
-      const bookId = '61d2d28331410a231151af38'
-      const book = {
-        title: "abc",
-        author: "pqr"
-      }
-      chai.request(server)
-          .put('/update/'+ bookId)
-          .send(book)
-          .end((err, response) => {
-            response.should.have.status(200);
-            response.text.should.be.eq('Data updated successfully');
-            done();
-          })
-    });
-  });
-
-
-  describe('DELETE /delete/:id', function() {
-
-    it('It should delete existing book', (done) => {
-
-      const bookId = '61d2d28331410a231151af38';
-      chai.request(server)
-          .delete('/delete/'+ bookId)
-          .end((err, response) => {
-            response.should.have.status(200);
-            response.text.should.be.eq('Data deleted successfully');
-            done();
-          })
-    });
-  });
-
 });
